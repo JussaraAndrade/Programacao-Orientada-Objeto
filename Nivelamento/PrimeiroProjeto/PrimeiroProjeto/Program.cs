289e059fa14710ad7352e1d47a77be67ceda654a
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-10-23 18:39:32 -0300

    Estrutura básica de um programa

---
+++ Nivelamento/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Referência do Console.WriteLine*/
+using System;
+
+/*Declaração de namespace; é um agrupamento de classes relacionadas*/
+namespace PrimeiroProjeto
+{
+    /*Declaração de classe; Todo código fonte tem que tá dentro da classe*/
+    class Program
+    {
+        /*Membros da classe*/
+        static void Main(string[] args) // Ponto de entrada
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
