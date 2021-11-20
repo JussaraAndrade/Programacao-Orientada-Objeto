755b3dbc5d8955b78bdc5887bf385148b7602ce7
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-11-19 22:26:14 -0300

    criação método para reaproveitamento e delegação

--- Orientação Objeto Básico/ProgramacaoObjetoBasico/ProgramacaoObjetoBasico/Program.cs
+++ Orientação Objeto Básico/ProgramacaoObjetoBasico/ProgramacaoObjetoBasico/Program.cs
@@ -17,69 +17,65 @@ namespace ProgramacaoObjetoBasico
             Duas váriaveis compostas: x e y
             */
 
-            //Triangulo x, y; // Duas variáveis do tipo Triangulo
+            // A classe Triangulo é um Tipo personalizados
+            Triangulo x, y; // Duas variáveis do tipo Triangulo
 
             //// Instanciar
-            //x = new Triangulo(); // Objeto x
-            //y = new Triangulo(); // Objeto y
+            x = new Triangulo(); // Objeto x
+            y = new Triangulo(); // Objeto y
 
 
-            //Console.WriteLine("Entre com as medidas do triângulo X: ");
-            //x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            //x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            //x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            //Console.WriteLine("Entre com as medidas do triângulo Y: ");
-            //y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            //y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            //y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+            double areaX = x.Area();
 
-            //double p = (x.A + x.B + x.C) / 2.0;
-            //// area = √p(p-a)(p-b)(p-c)
-            //double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+            double areaY = y.Area();
 
-            //// p = a + b + c/2 
-            //p = (y.A + y.B + y.C) / 2.0;
-            //double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+            Console.WriteLine("Área de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
 
-            //Console.WriteLine("Área de x = " +areaX.ToString("F4", CultureInfo.InvariantCulture));
-            //Console.WriteLine("Área de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
 
-            //if(areaX > areaY)
-            //{
-            //    Console.WriteLine("Maior área: X");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Maior área: Y");
-            //}
+            Console.WriteLine("-------------------------------------------");
 
-            //Console.WriteLine("-------------------------------------------");
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
 
-            //Pessoa p1 = new Pessoa();
-            //Pessoa p2 = new Pessoa();
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Console.WriteLine("Nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            p1.Idade = int.Parse(Console.ReadLine());
 
-            //Console.WriteLine("Dados da primeira pessoa: ");
-            //Console.WriteLine("Nome: ");
-            //p1.Nome = Console.ReadLine();
-            //Console.WriteLine("Idade: ");
-            //p1.Idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dados da segunda pessoa: ");
+            Console.WriteLine("Nome: ");
+            p2.Nome = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            p2.Idade = int.Parse(Console.ReadLine());
 
-            //Console.WriteLine("Dados da segunda pessoa: ");
-            //Console.WriteLine("Nome: ");
-            //p2.Nome = Console.ReadLine();
-            //Console.WriteLine("Idade: ");
-            //p2.Idade = int.Parse(Console.ReadLine());
-
-            //if (p1.Idade > p2.Idade)
-            //{
-            //    Console.WriteLine("Pessoa mais velha: " +p1.Nome);
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Pessoa mais velha: " + p2.Nome);
-            //}
+            if (p1.Idade > p2.Idade)
+            {
+                Console.WriteLine("Pessoa mais velha: " + p1.Nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: " + p2.Nome);
+            }
 
             Funcionario f1 = new Funcionario();
             Funcionario f2 = new Funcionario();
--- Orientação Objeto Básico/ProgramacaoObjetoBasico/ProgramacaoObjetoBasico/Triangulo.cs
+++ Orientação Objeto Básico/ProgramacaoObjetoBasico/ProgramacaoObjetoBasico/Triangulo.cs
@@ -1,11 +1,21 @@
-﻿namespace ProgramacaoObjetoBasico
+﻿using System; // Importação de dependências
+
+namespace ProgramacaoObjetoBasico // Namespace da clase
 {
-    class Triangulo
+    class Triangulo // Nome da Classe
     {
         // Atributos A, B e C - Definição do triângulo:
+        // prefixo public; indica que o método pode ser usado em outros arquivos
         public double A; // Dado A do tipo double
         public double B; // Dado B do tipo double
         public double C; // Dado C do tipo double
-        // A classe Triangulo é um Tipo personalizados
+        
+        // Método é uma função dentro da classe, tipo da classe é double
+        public double Area() // Nenhum parametro de entrada
+        {
+            double p = (A + B + C) / 2.0;
+            // area = √p(p-a)(p-b)(p-c)
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C)); // Corpo do método
+        }
     }
 }
